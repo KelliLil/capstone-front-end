6600f3d1a6ece203b7952f1989037dd643d3a33c
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-13 12:04:31 -0600

    import font

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { CssBaseline } from "@mui/material";
 import {
   createTheme,
@@ -8,10 +12,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
