5595b4b563bc8a714bfd6d5bd61872ab310b2a9c
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-13 11:56:51 -0600

    Render card for random restaurant

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,3 @@
-export { default as userApi } from "./user-api.js";
+export { default as cuisineApi } from "./cuisine-api";
+export { default as restaurantApi } from "./restaurant-api";
+export { default as userApi } from "./user-api";
