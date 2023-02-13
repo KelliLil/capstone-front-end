0f90889adf2d81410d7e3b558d85e786bc483be8
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-13 12:00:53 -0600

    Adjust browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import {
-  createBrowserRouter,
-  redirect,
-  RouterProvider,
-} from "react-router-dom";
+import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import AuthContext from "./context/auth";
+import Home from "./routes/home";
 import SignIn from "./routes/sign-in";
-import { userApi } from "./services";
+import { cuisineApi } from "./services";
+import { getUserFromToken } from "./utils";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Welcome</h1>,
-  },
-  {
-    path: "/sign-in",
-    element: <SignIn />,
-    async action({ request }) {
-      const fd = await request.formData();
-      await userApi.signIn(Object.fromEntries(fd));
-
-      // * Redirect to home page
-      return redirect("/");
-    },
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader() {
+          return cuisineApi.index();
+        },
+      },
+      {
+        path: "/sign-in",
+        element: <SignIn />,
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [user, setUser] = useState(getUserFromToken());
+  return (
+    <AuthContext.Provider value={[user, setUser]}>
+      <RouterProvider router={router} />
+    </AuthContext.Provider>
+  );
 }
-
 export default App;
