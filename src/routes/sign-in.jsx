981bf97cb7c802908cf510b760b74f22b039e54a
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-13 11:47:34 -0600

    style:

--- src/routes/sign-in.jsx
+++ src/routes/sign-in.jsx
@@ -3,18 +3,28 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Alert } from "@mui/material";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import { useState } from "react";
+
+import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Form, useSubmit } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import InputText from "../components/input-text";
+import AuthContext from "../context/auth";
+import { userApi } from "../services";
+import { getUserFromToken } from "../utils";
 
-// TODO: Go home if there is a token in localStorage
 export default function SignIn() {
   const [isRegistering, setIsRegistering] = useState(true);
 
-  // Manually set up the submit handler
-  const submit2Router = useSubmit();
+  const [user, setUser] = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
 
   const formSchema = yup.object({
     username: yup.string().required("Username is required"),
@@ -28,6 +38,7 @@ export default function SignIn() {
 
   const {
     register,
+    handleSubmit,
     formState: { errors },
     control,
     reset,
@@ -38,78 +49,68 @@ export default function SignIn() {
 
   return (
     <>
-      <div className="h-screen bg-gray-400 text-rose-600">
-        <Container component="main" maxWidth="xs">
-          <h1 className="p-9 text-center font-serif capitalize">Iron Out</h1>
-          <Form
-            className="mt-4 flex flex-col items-center gap-y-4"
-            onSubmit={(e) => {
-              e.preventDefault();
-              const fd = new FormData(e.target);
+      <Container component="main" maxWidth="xs">
+        <h1 className="text-center capitalize">Sign in</h1>
+        <Form
+          className="mt-4 flex flex-col items-center gap-y-4"
+          onSubmit={handleSubmit(async (data) => {
+            // TODO: Build Error Route
+            const { token } = await userApi.signIn(data, isRegistering);
+            localStorage.setItem("token", token);
 
-              // * ⚠️ FormData doesn't support boolean
-              // We will send it as a string, or not send it at all (if it's false) 🤓
-              isRegistering && fd.append("isRegistering", isRegistering);
+            setUser(getUserFromToken(token));
 
-              // Let the router handle the submit
-              submit2Router(
-                fd,
-
-                // * Need this to trigger the action in the router
-                { method: "POST" }
-              );
-            }}
+            // TODO: Navigate to link builder
+            navigate("/");
+          })}
+        >
+          <InputText label="Username" id="username" register={register}>
+            {errors.username && (
+              <Alert severity="error">{errors.username.message}</Alert>
+            )}
+          </InputText>
+          <InputText
+            label="Password"
+            id="password"
+            type="password"
+            register={register}
           >
-            <InputText label="Username" id="username" register={register}>
-              {errors.username && (
-                <Alert severity="error">{errors.username.message}</Alert>
-              )}
-            </InputText>
+            {errors.password && (
+              <Alert severity="error">{errors.password.message} </Alert>
+            )}
+          </InputText>
+          {isRegistering && (
             <InputText
-              label="Password"
-              id="password"
+              label="Confirm Password"
+              id="confirmPassword"
               type="password"
               register={register}
             >
-              {errors.password && (
-                <Alert severity="error">{errors.password.message} </Alert>
+              {errors.confirmPassword && (
+                <Alert severity="error">{errors.confirmPassword.message}</Alert>
               )}
             </InputText>
-            {isRegistering && (
-              <InputText
-                label="Confirm Password"
-                id="confirmPassword"
-                type="password"
-                register={register}
-              >
-                {errors.confirmPassword && (
-                  <Alert severity="error">
-                    {errors.confirmPassword.message}
-                  </Alert>
-                )}
-              </InputText>
-            )}
-            <div className="my-4 flex flex-col items-center gap-y-8">
-              <Button type="submit" fullWidth variant="contained">
-                {isRegistering ? "Sign Up" : "Login"}
-              </Button>
-              <Button
-                // * Avoid the HTML 'reset' for React Hook Form instead use the reset() method ☝️
-                type="button"
-                onClick={() => {
-                  reset();
-                  setIsRegistering((prev) => !prev);
-                }}
-              >
-                {isRegistering
-                  ? "Already have an account?"
-                  : "Don't have an account?"}
-              </Button>
-            </div>
-          </Form>
-        </Container>
-        <DevTool control={control} /> {/* set up the dev tool */}
-      </div>
+          )}
+          <div className="my-4 flex flex-col items-center gap-y-8">
+            <Button type="submit" fullWidth variant="contained">
+              {isRegistering ? "Sign Up" : "Login"}
+            </Button>
+            <Button
+              // * Avoid the HTML 'reset' for React Hook Form instead use the reset() method ☝️
+              type="button"
+              onClick={() => {
+                reset();
+                setIsRegistering((prev) => !prev);
+              }}
+            >
+              {isRegistering
+                ? "Already have an account?"
+                : "Don't have an account?"}
+            </Button>
+          </div>
+        </Form>
+      </Container>
+      <DevTool control={control} /> {/* set up the dev tool */}
     </>
   );
 }
--- src/routes/welcome.jsx
+++ src/routes/welcome.jsx
@@ -1,12 +1,108 @@
-import { DevTool } from "@hookform/devtools";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Alert } from "@mui/material";
+import ExploreIcon from "@mui/icons-material/Explore";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
+import Slider from "@mui/material/Slider";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Form, useSubmit } from "react-router-dom";
-import * as yup from "yup";
-import InputText from "../components/input-text";
+import { useLoaderData } from "react-router-dom";
+import RestaurantCard from "../components/restaurant/restaurant-card";
+import { restaurantApi } from "../services";
 
+export default function Home() {
+  const [restaurant, setRestaurant] = useState(null);
 
+  const cuisines = useLoaderData();
+
+  // For Autocomplete, we need just the strings
+  const labeledCuisines = cuisines.map((cuisine) => cuisine.name);
+
+  return (
+    <Container component="main" maxWidth="xs" className="mb-8">
+      {/* 
+      
+      // TODO: Do this 💩 later. 😅
+      <p>
+        Just for U? 🆒 👇🏾 Fill out the form below and you&apos;ll be on your
+        way. 😋
+      </p>
+      <p>
+        Want to coordinate with a group? Head <Link to="/sign-in>">here</Link>{" "}
+        to register an account 🛂 and start sending out links to your friends.
+        Or enemies 🤛🏾 to snare them into a trap 🚩 👿 🤣.
+      </p> */}
+
+      {/* TODO: Add some 'Link's to 'dumb' routes to explain and 'sell' 😵‍💫 this thing! */}
+      <form
+        className="my-16 flex flex-col gap-y-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const fd = new FormData(e.target);
+          const data = Object.fromEntries(fd);
+
+          restaurantApi.show(data).then((restaurant) => {
+            setRestaurant(restaurant);
+          });
+        }}
+      >
+        <Autocomplete
+          disablePortal
+          options={labeledCuisines}
+          renderInput={
+            // 'params' is used by MUI to control the input value
+            (params) => (
+              <TextField
+                {...params}
+                label="What cuisine R U in the mood for? 😋"
+                id="cuisine"
+                name="category"
+              />
+            )
+          }
+        />
+
+        {/* TODO: Allow other inputs such as city 🏙️. */}
+        {/* TODO: Use geolocation 🤓 🗺️. */}
+        <TextField
+          label="Where do you want to head? (5-Digit Zip Code)"
+          id="zip"
+          name="location"
+          // TODO: Improve validation experience 🚸.
+          inputProps={{ pattern: "[0-9]{5}" }}
+          required
+        />
+
+        <div className="flex items-center gap-x-4">
+          <NearMeIcon />
+          <Slider
+            aria-label="Range"
+            defaultValue={10}
+            getAriaValueText={(value) => `${value} miles`}
+            marks
+            min={5}
+            max={25}
+            step={5}
+            valueLabelDisplay="on"
+            id="radius"
+            name="radius"
+          />
+          <ExploreIcon />
+        </div>
+
+        <Button
+          variant="contained"
+          color="success"
+          type="submit"
+          className="max-w-max self-center"
+        >
+          Go!
+        </Button>
+      </form>
+
+      {restaurant && <RestaurantCard restaurant={restaurant} />}
+
+      {/* TODO: 🗺️ */}
+    </Container>
+  );
+}
