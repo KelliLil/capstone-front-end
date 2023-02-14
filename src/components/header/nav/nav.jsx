c7e04d345f42ab945bcb16dccafc1ee229787a4c
Kelli Lilly <kelli.lilly05@gmail.com>
2023-02-14 10:03:17 -0600

    style: :lipstick:

--- src/components/header/nav/nav.jsx
+++ src/components/header/nav/nav.jsx
@@ -1,9 +1,13 @@
+import LinkHome from "@components/link-home";
 import SignInOut from "./sign-in-out";
 
 export default function Nav() {
   return (
-    <nav className="flex justify-around bg-slate-100 py-1 text-slate-900">
-      <ul>
+    <nav className=" bg-slate-100 py-1 px-4 text-slate-900">
+      <ul className="flex items-center justify-between">
+        <li>
+          <LinkHome />
+        </li>
         <SignInOut />
       </ul>
     </nav>
